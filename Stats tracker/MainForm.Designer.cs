ace0708a3c4f996e07b7da23d3acff0436f3a18c
Qarnix <joris.wijnen97@gmail.com>
2017-06-06 13:51:04 +0200

    V1.0

---
+++ Stats tracker/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Stats_tracker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLeftBar = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelLeftBar
+            // 
+            this.panelLeftBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeftBar.Location = new System.Drawing.Point(0, 0);
+            this.panelLeftBar.Name = "panelLeftBar";
+            this.panelLeftBar.Size = new System.Drawing.Size(200, 1041);
+            this.panelLeftBar.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.panelLeftBar);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hot Water Vapor™";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLeftBar;
+    }
+}
+
---
+++ Stats tracker/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Stats_tracker
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
